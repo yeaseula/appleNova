1248dd18f2a9ff7607ff70b5252762fe797760fc
yeaseula <kminea@naver.com>
2025-09-05 19:48:18 +0900

    NavBar tailwind.css 변경

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,13 +3,13 @@ import {Link} from 'react-router-dom';
 
 export default function NavBar(){
     return (
-        <nav className="navbar">
-            <ul>
-                <li><Link to="/">iPhone</Link></li>
-                <li><Link to="/ipad">iPad</Link></li>
-                <li><Link to="/airpod">AirPods</Link></li>
-                <li><Link to="/airmax">AirMax</Link></li>
-                <li><Link to="/macbook">MacBook</Link></li>
+        <nav className="navbar fixed top-15 left-[50%] translate-x-[-50%]">
+            <ul className="flex justify-center gap-5">
+                <li className="bg-purple-400 hover:bg-purple-600 rounded shadow-lg shadow-indigo-500/50 transition"><Link to="/" className="inline-block text-white text-lg pt-1 pb-1 pl-5 pr-5">iPhone</Link></li>
+                <li className="bg-purple-400 hover:bg-purple-600 rounded shadow-lg shadow-indigo-500/50 transition"><Link to="/ipad" className="inline-block text-white text-lg pt-1 pb-1 pl-5 pr-5">iPad</Link></li>
+                <li className="bg-purple-400 hover:bg-purple-600 rounded shadow-lg shadow-indigo-500/50 transition"><Link to="/airpod" className="inline-block text-white text-lg pt-1 pb-1 pl-5 pr-5">AirPods</Link></li>
+                <li className="bg-purple-400 hover:bg-purple-600 rounded shadow-lg shadow-indigo-500/50 transition"><Link to="/airmax" className="inline-block text-white text-lg pt-1 pb-1 pl-5 pr-5">AirMax</Link></li>
+                <li className="bg-purple-400 hover:bg-purple-600 rounded shadow-lg shadow-indigo-500/50 transition"><Link to="/macbook" className="inline-block text-white text-lg pt-1 pb-1 pl-5 pr-5">MacBook</Link></li>
             </ul>
         </nav>
     )
