efbe2cdbcc0d53b7068af7bd9569f5840bbafa9f
yeaseula <kminea@naver.com>
2025-09-05 23:41:27 +0900

    🏗️ React Three Fiber로   변경

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 export default function NavBar(){
     return (
-        <nav className="navbar fixed top-15 left-[50%] translate-x-[-50%]">
+        <nav className="navbar fixed top-15 left-[50%] translate-x-[-50%] z-99">
             <ul className="flex justify-center gap-5">
                 <li className="bg-purple-400 hover:bg-purple-600 rounded shadow-lg shadow-indigo-500/50 transition"><Link to="/" className="inline-block text-white text-lg pt-1 pb-1 pl-5 pr-5">iPhone</Link></li>
                 <li className="bg-purple-400 hover:bg-purple-600 rounded shadow-lg shadow-indigo-500/50 transition"><Link to="/ipad" className="inline-block text-white text-lg pt-1 pb-1 pl-5 pr-5">iPad</Link></li>
--- pages/Showroom.jsx
+++ pages/Showroom.jsx
@@ -1,205 +1,168 @@
 import React from "react";
-import { useEffect } from "react";
+import { Suspense, useMemo } from 'react';
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, Environment, useGLTF } from "@react-three/drei";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
-export default function Showroom({product}) {
+import { TextureLoader } from "three";
+import { RGBELoader } from "three-stdlib";
 
-    const modelMap = {
-        iphone: {
-            modalPath: '/src/assets/glb/iphone.glb',
-            position: [0,0,0],
-            scale:[2,2,2],
-            rotation:[0,(-Math.PI / 2) + 0.3, 0],
-            lightpower:7
-        },
-        airpod: {
-            modalPath: '/src/assets/glb/airpod.glb',
-            position: [0,-0.7,0],
-            scale:[35,35,35],
-            rotation:[0,(-Math.PI / 2) - 1, 0],
-            lightpower:3
-        },
-        ipad: {
-            modalPath: '/src/assets/glb/ipad.glb',
-            position: [0,-1.8,0],
-            scale:[0.04,0.04,0.04],
-            rotation:[0,(-Math.PI / 2), 0],
-            lightpower:8
-        },
-        airmax: {
-            modalPath: '/src/assets/glb/airpods_max.glb',
-            position: [0,-1.5,0],
-            scale:[2.1,2.1,2.1],
-            rotation:[0,(-Math.PI / 2), 0],
-            lightpower:1
-        },
-        macbook: {
-            modalPath: '/src/assets/glb/macbook.glb?',
-            position: [0,-0.9,0],
-            scale:[8.5,8.5,8.5],
-            rotation:[0,(-Math.PI / 2) + 0.3, 0],
-            lightpower:3
-        }
+const modelMap = {
+    iphone: {
+        modalPath: '/src/assets/glb/iphone.glb',
+        position: [0,0,0],
+        scale:[1.8,1.8,1.8],
+        rotation:[0,(-Math.PI / 2) + 0.2, 0],
+        lightpower:7
+    },
+    airpod: {
+        modalPath: '/src/assets/glb/airpod.glb',
+        position: [0,-0.5,0],
+        scale:[35,35,35],
+        rotation:[0,(-Math.PI / 2) - 1, 0],
+        lightpower:3
+    },
+    ipad: {
+        modalPath: '/src/assets/glb/ipad.glb',
+        position: [0,-1.6,0],
+        scale:[0.04,0.04,0.04],
+        rotation:[0,(-Math.PI / 2), 0],
+        lightpower:8
+    },
+    airmax: {
+        modalPath: '/src/assets/glb/airpods_max.glb',
+        position: [0,-1.5,0],
+        scale:[2.2,2.2,2.2],
+        rotation:[0,(-Math.PI / 2), 0],
+        lightpower:1
+    },
+    macbook: {
+        modalPath: '/src/assets/glb/macbook.glb?',
+        position: [0,-1.1,0],
+        scale:[8.4,8.4,8.4],
+        rotation:[0,(-Math.PI / 2) + 0.3, 0],
+        lightpower:3
     }
-    const modalPath = modelMap[product] || modelMap['iphone']
-    useEffect(() => {
-        // 씬, 카메라, 렌더러
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color(0x000000);
+}
 
-        const camera = new THREE.PerspectiveCamera(
-            45,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            1000
-        );
-        camera.position.set(5, 0, 5);
-        const renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setSize(window.innerWidth, window.innerHeight);
+function ProductCall({modalPath,position,scale,rotation}) {
+    const gltf = useGLTF(modalPath);
+    const roughMap = useLoader(TextureLoader,'/image/metal3.jpg')
+    useMemo(() => {
+        gltf.scene.traverse((o) => {
+        if (!o.isMesh) return;
+        const n = o.name.toLowerCase();
 
-        //shadow 그림자
-        renderer.shadowMap.enabled = true;
-        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-        const container = document.getElementById("three-root");
-        container.appendChild(renderer.domElement);
-
-        // OrbitControls
-        const controls = new OrbitControls(camera, renderer.domElement);
-        controls.update();
-
-        const ambient = new THREE.AmbientLight(0xffffff, 2.5);
-        scene.add(ambient);
-
-        const spot = new THREE.SpotLight(0xffffff, modalPath.lightpower);
-        spot.position.set(5, 10, 5); // 위, 사선 등 원하는 위치
-        spot.target.position.set(modalPath.position[0],modalPath.position[1],modalPath.position[2]); // 제품 위치
-        spot.castShadow = true;
-        scene.add(spot);
-        scene.add(spot.target);
-
-        // GLB 로드
-        const loader = new GLTFLoader();
-        const textureLoader = new THREE.TextureLoader();
-        const roughMap = textureLoader.load("/image/metal3.jpg");
-
-        loader.load(modalPath.modalPath, (gltf) => {
-            const appleProduct = gltf.scene;
-
-            appleProduct.traverse((o) => {
-                if (!o.isMesh) return;
-                const n = o.name.toLowerCase();
-
-                console.log(`Mesh name : ${o.name}`)
-
-                if (n.includes("basecolor")) {
-                    o.material = new THREE.MeshPhysicalMaterial({
-                        color: 0x948f85,
-                        metalness: 0,
-                        roughness: 0.2, // 0에 가까울수록 유리광택, 1에 가까울수록 매트
-                        clearcoat: 0.1, // 1에 가까울수록 투명 코팅느낌
-                        clearcoatRoughness: 0.2, // 코팅의 거칠기
-                        transmission: 0, //1에 가까울수록 투명도 상승
-                        thickness: 0.2, // 재질의 두께
-                        ior: 1.5, //굴절률
-                    });
-                } else if (n.includes("back") || n.includes("glass")) {
-                    o.material = new THREE.MeshPhysicalMaterial({
-                        color: 0xffffff,
-                        metalness: 0,
-                        roughness: 0,
-                        transmission: 1,
-                        thickness: 0.3,
-                        opacity: 0.4,
-                        clearcoat: 1,
-                        clearcoatRoughness: 0.2,
-                        ior: 1.5,
-                    })
-                } else if (n.includes("frame") || n.includes("edge") || n.includes("metal")) {
-                    o.material = new THREE.MeshStandardMaterial({
-                        color: 0x846d3f,
-                        metalness: 1,
-                        roughness: 0.2,
-                        clearcoat: 0,
-                        transmission: 0,
-                        roughnessMap: roughMap,
-                    });
-                } else if(n.includes("logo")) {
-                    o.material = new THREE.MeshPhysicalMaterial({
-                        color: 0x89847e,
-                        metalness: 0.4,
-                        roughness: 0.2,
-                        clearcoat: 0,
-                        roughnessMap: roughMap,
-                    });
-                }
-
-                if(n.includes("plastic")) {
-                    o.material = new THREE.MeshPhysicalMaterial({
-                        color: 0x757575,
-                        metalness: 0,
-                        roughness: 0.2,
-                        ior: 1.5, //굴절률
-                    });
-                }
-                o.castShadow = true;
-                o.receiveShadow = false;
+        if (n.includes("basecolor")) {
+            o.material = new THREE.MeshPhysicalMaterial({
+            color: 0x948f85,
+            metalness: 0,
+            roughness: 0.2,
+            clearcoat: 0.1,
+            clearcoatRoughness: 0.2,
+            transmission: 0,
+            thickness: 0.2,
+            ior: 1.5,
             });
-
-            appleProduct.scale.set(modalPath.scale[0],modalPath.scale[1],modalPath.scale[2])
-            appleProduct.position.set(modalPath.position[0],modalPath.position[1],modalPath.position[2])
-            appleProduct.rotation.set(modalPath.rotation[0],modalPath.rotation[1],modalPath.rotation[2])
-            scene.add(appleProduct);
-            },
-            undefined,
-            (err) => console.error("GLB 로드 오류:", err)
-        );
-
-        // 환경맵
-        const pmrem = new THREE.PMREMGenerator(renderer);
-        new RGBELoader().load("/src/assets/hdr/artist_workshop.hdr", (hdrTex) => {
-            const envMap = pmrem.fromEquirectangular(hdrTex).texture;
-            scene.environment = envMap;
+        } else if (n.includes("back") || n.includes("glass")) {
+            o.material = new THREE.MeshPhysicalMaterial({
+            color: 0xffffff,
+            metalness: 0,
+            roughness: 0,
+            transmission: 1,
+            thickness: 0.3,
+            opacity: 0.6,
+            clearcoat: 1,
+            clearcoatRoughness: 0.2,
+            ior: 1,
+            });
+        } else if (n.includes("frame") || n.includes("edge") || n.includes("metal")) {
+            o.material = new THREE.MeshStandardMaterial({
+            color: 0xba9f69,
+            metalness: 1,
+            roughness: 0.2,
+            clearcoat: 0,
+            transmission: 0,
+            roughnessMap: roughMap,
+            });
+        } else if (n.includes("logo")) {
+            o.material = new THREE.MeshPhysicalMaterial({
+            color: 0xba9f69,
+            metalness: 0.4,
+            roughness: 0.2,
+            clearcoat: 0,
+            roughnessMap: roughMap,
+            });
+        }
+        if (n.includes("plastic")) {
+            o.material = new THREE.MeshPhysicalMaterial({
+            color: 0x9a9999,
+            metalness: 0,
+            roughness: 0.2,
+            ior: 1.5,
+            });
+        }
+        o.castShadow = true;
+        o.receiveShadow = false;
         });
+    }, [gltf]);
 
-        //바닥
-        const floorGeometry = new THREE.PlaneGeometry(20,20)
-        const floorMat = new THREE.ShadowMaterial({
-            color: 0xfafafa,   // 그림자 색
-            opacity: 0.1       // 그림자 진하기
-        });
-        const floor = new THREE.Mesh(floorGeometry,floorMat)
-        floor.rotation.x = -Math.PI / 2 //90도
-        floor.position.y= -2;
-        floor.receiveShadow = true;
-        scene.add(floor)
+    return (
+        <group scale={scale} position={position} rotation={rotation}>
+            <primitive
+            object={gltf.scene}
+            castShadow
+            ></primitive>
+        </group>
+    )
+}
 
-        // 렌더 루프
-        const animate = () => {
-            requestAnimationFrame(animate);
-            controls.update();
-            renderer.render(scene, camera);
-        };
-        animate();
 
-        // 리사이즈 대응
-        const handleResize = () => {
-            camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
-            renderer.setSize(window.innerWidth, window.innerHeight);
-        };
-        window.addEventListener("resize", handleResize);
+export default function Showroom({product}) {
+    const modalPath = modelMap[product] || modelMap['iphone'];
+    const LightPower = modelMap[product].lightpower || modelMap['iphone'].lightpower;
 
-        // cleanup
-        return () => {
-            window.removeEventListener("resize", handleResize);
-            renderer.dispose();
-            container.removeChild(renderer.domElement);
-        };
-        }, [modelMap]);
-
-    return <div id="three-root" style={{ width: "100vw", height: "100vh" }} />
+    return (
+        <div className="w-[100vw] h-[100vh]">
+            <Canvas shadows
+            camera={{ position:[5,0,5], fov:45 }}
+            key={product}
+            className="w-[100vw] h-[100vh]"
+            style={{ background:'#fafafa' }}>
+                <ambientLight intensity={2.5} color={'0xffffff'}></ambientLight>
+                <spotLight
+                position={[5,10,5]}
+                intensity={LightPower}
+                angle={0.3}
+                penumbra={1}
+                color={'0xffffff'}
+                castShadow
+                target-position={modalPath.position}
+                ></spotLight>
+                <Suspense fallback={null}>
+                    <ProductCall
+                    modalPath={modalPath.modalPath}
+                    position={modalPath.position}
+                    scale={modalPath.scale}
+                    rotation={modalPath.rotation}
+                    ></ProductCall>
+                    <Environment
+                        files="/src/assets/hdr/artist_workshop.hdr"
+                        background={false}
+                        path=""
+                        preset={null}
+                    />
+                </Suspense>
+                <mesh
+                rotation={[-Math.PI / 2,0,0]}
+                position={[0,-2,0]}
+                receiveShadow
+                >
+                    <planeGeometry args={[20, 20]} />
+                    <shadowMaterial color={0xfafafa} opacity={0.1} />
+                </mesh>
+                <OrbitControls></OrbitControls>
+            </Canvas>
+        </div>
+    )
 
 }
